66ff6c27f9e04add4a11449919b364625767c43a
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-08-29 20:13:35 +0300

    Add linter setup files and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-	mode: 'development',
+module.exports = {
+  mode: 'development',
   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-	 },
-	devServer: {
+    index: './src/index.js',
+  },
+  devServer: {
     static: './dist',
   },
-   plugins: [
-     new HtmlWebpackPlugin({
-			 template: './src/index.html',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-	 },
-	module: {
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
     ],
-	 },
-	optimization: {
+  },
+  optimization: {
     runtimeChunk: 'single',
   },
- };
+};

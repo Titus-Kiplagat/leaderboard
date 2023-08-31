8c0218fa58c688ca07fb328b247b06533fe8eebd
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-08-31 22:05:37 +0300

    Update index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import './style.css';
 import submitForm from './modules/submitForm.js';
 import handleSubmitButton from './modules/handleSubmitButton.js';
-import fetchScores from './modules/fetchScores.js';
 import refreshScores from './modules/refreshScores.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   submitForm();
   handleSubmitButton();
   refreshScores();
-  fetchScores();
 });
